8958f1152a6fd5c2090a64e10d18d2d8d05a757e
jessicaluhrmann <jessica.luhrmann@xplortechnologies.com>
2023-10-19 11:44:11 +1000

    Add project files.

---
+++ ArchitectArithmetic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rectangleArea = Rectangles(2500, 1500);
+            var circleArea = Circles(375) / 2;
+            var triangleArea = Triangles(750, 500);
+            var totalArea = rectangleArea + circleArea + triangleArea;
+            var areaCost = Math.Round(totalArea * 180, 2);
+
+            Console.WriteLine($"The total cost of flooring in Mexican Pesos will be {areaCost}.");
+        }
+        public static double Rectangles(double length, double width)
+        {
+            return length * width;
+        }
+        public static double Circles(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        public static double Triangles(double bottom, double height)
+        {
+            return 0.5 * bottom * height;
+        }
+    }
+}
